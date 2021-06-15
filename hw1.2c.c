5e2346fdd809b147086b9c40038e80a34caf0e32
valdrin18 <72058592+valdrin18@users.noreply.github.com>
2021-06-15 22:31:39 +0200

    Delete hw1.2c.c

--- hw1.2c.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-    int result;
-    result = (2 + 7) * 9 / 3;
-    printf("The result is %d\n",result);
-    return 0;
-}
-//we had to call variable result at print command
